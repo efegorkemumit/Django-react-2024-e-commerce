83124d728da3aeb38b83549e144bdd9439a84263
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-21 15:28:10 +0300

    Swipper js

--- frontend-vite-project/src/components/Slider.tsx
+++ frontend-vite-project/src/components/Slider.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
 
 function Slider() {
   return (
     <div>
-Slider
+ <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <div className=''>
+                <img src='img/slider/banner1.png'/>
+            </div>
+
+
+
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className=''>
+                <img src='img/slider/banner2.png'/>
+            </div>
+
+        </SwiperSlide>
+      
+      </Swiper>
 
     </div>
   )
