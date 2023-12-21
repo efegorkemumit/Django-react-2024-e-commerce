cf442e79c0b36c14efb23ae16f6d401aebb12308
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-21 19:47:22 +0300

    Slider Design son

--- frontend-vite-project/src/components/Slider.tsx
+++ frontend-vite-project/src/components/Slider.tsx
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import '../my.css'
 import anime from 'animejs/lib/anime.es.js';
 
 
