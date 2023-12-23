300b9b6fdaca3bd74a41e5c21f4a2df800be301d
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-23 10:00:50 +0300

    Category 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product',
 ]
 
 MIDDLEWARE = [
@@ -109,6 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL ="media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+def category_image_path(instance, filename):
+    return f"category/{instance.id}/server/{filename}"
+
+class Category(models.Model):
+    title= models.CharField(max_length=100)
+    slug =AutoSlugField(populate_from='title')
+    photo=models.ImageField(
+        upload_to = category_image_path,
+        null=True,
+        blank=True,
+         
+    )
+    menuicon=models.CharField(max_length=200,
+     help_text="Font Awesome sitesinden alabilirsin icon örnek kullanım : fa-regular fa-circle-xmark   fa-solid fa-circle-xmark")
+
+    def __str__(self):
+        return self.title
