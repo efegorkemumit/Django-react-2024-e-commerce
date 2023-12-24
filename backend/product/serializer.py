d18095872a9d8d49db5ed9155a032cd219b62cc8
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-24 13:55:25 +0300

    product_count

--- backend/product/serializer.py
+++ backend/product/serializer.py
@@ -4,11 +4,17 @@ from .models import Category, Brand , Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
     class Meta:
         model = Category
         fields = '__all__'
+    def get_product_count(self,obj):
+        return obj.product_set.count()
 
 class BrandSerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
     class Meta:
         model = Brand
         fields = '__all__'
+    def get_product_count(self,obj):
+        return obj.product_set.count()
