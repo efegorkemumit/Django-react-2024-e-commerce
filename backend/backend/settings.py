0ebd7e13a72630acd67ed90ac19e08068afb38da
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-25 16:46:52 +0300

    cors headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'drf_spectacular',
     'drf_spectacular_sidecar', 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,6 +135,12 @@ MEDIA_URL ="media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    
+]
+
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Efe Görkem Ümit',
