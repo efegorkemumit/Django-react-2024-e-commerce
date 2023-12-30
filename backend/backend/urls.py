bf3faa09be9e349932bfdb3fd599010a854d7f25
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2023-12-30 15:00:33 +0300

    user login

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,9 +27,10 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('api/customer/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/customer/create/', CustomerUserRegisterView.as_view(), name='customer-register'),
 
 
